2627f92b851247e5c336df0cb80d5b6cf486a67f
Mathias Kub <git@makubi.at>
2012-06-26 23:40:04 +0200

    Model angepasst

--- src/main/java/at/makubi/webuntis/model/SchoolClass.java
+++ src/main/java/at/makubi/webuntis/model/SchoolClass.java
@@ -2,4 +2,26 @@ package at.makubi.webuntis.model;
 
 public class SchoolClass {
 
+	private String name;
+	private String foreColor;
+	private String backColor;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getForeColor() {
+		return foreColor;
+	}
+	public void setForeColor(String foreColor) {
+		this.foreColor = foreColor;
+	}
+	public String getBackColor() {
+		return backColor;
+	}
+	public void setBackColor(String backColor) {
+		this.backColor = backColor;
+	}
 }
